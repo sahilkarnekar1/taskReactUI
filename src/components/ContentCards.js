189b52b869a6ed0c0553686db47ca9aed3980be6
Sahil Karnekar <115504306+sahilkarnekar1@users.noreply.github.com>
2024-04-22 00:19:25 +0530

    Update ContentCards.js

--- src/components/ContentCards.js
+++ src/components/ContentCards.js
@@ -59,7 +59,7 @@ const ContentCards = () => {
           <div className="card setCardW swiper-slide">
             <img className="card-img-top" src={markImg} alt="Card image cap" />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Swipe Left Or Right</h5>
               <p className="card-text">
                 Some quick example text to build on the card title and
                 make up the bulk of the card's content.
